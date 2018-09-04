1565513c68427b1704f71f34af75806865c0e124
alice T <alicewtien@gmail.com>
2018-09-04 01:04:48 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarves)
-  dwarves.collect { |names| names }
+  dwarves.collect { |names| puts names } #collect returns new array
 end
 
 def summon_captain_planet# code an argument here
