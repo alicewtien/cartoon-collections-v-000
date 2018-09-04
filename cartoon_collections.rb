3030c4e2b44700c02ee8b6060dc5faa5232a0db7
alice T <alicewtien@gmail.com>
2018-09-04 01:11:07 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,9 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(elements)
-  
+  elements.map do |element|
+    "#{element.capitalize}!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
